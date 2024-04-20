d9dcced3c4bb0f7b82b699601240585f4e4b9822
krzysiu <krzysna@gmail.com>
2024-04-20 13:09:45 +0200

    SubDir

--- app/src/main/java/pl/edu/pg/mso/lab3/MainActivity.kt
+++ app/src/main/java/pl/edu/pg/mso/lab3/MainActivity.kt
@@ -3,13 +3,12 @@ package pl.edu.pg.mso.lab3
 import android.app.AlertDialog
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ListView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.RecyclerView
+import java.io.File
 import java.nio.charset.Charset
 
 class MainActivity : AppCompatActivity() {
@@ -23,12 +22,16 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        listFiles()
+        listFiles(applicationContext.filesDir)
         val builder = AlertDialog.Builder(this)
         findViewById<Button>(R.id.privFiles)?.setOnClickListener {
             val fileName: String = findViewById<TextView>(R.id.fileName).text.toString()
+
+            val dirName: String = findViewById<TextView>(R.id.dirName).text.toString()
+            val subDir = File(applicationContext.filesDir, dirName)
+
             val helper = FilesHelper()
-            val file = helper.preparePrivateFile(applicationContext, fileName)
+            val file = helper.preparePrivateFile(applicationContext, fileName, if (dirName != "Dir") dirName else null)
             val fileData = helper.readPrivateFile(file);
 
             findViewById<TextView>(R.id.fileContent).text = if (fileData != null) String(fileData) else ""
@@ -38,30 +41,40 @@ class MainActivity : AppCompatActivity() {
         }
         findViewById<Button>(R.id.saveBtn)?.setOnClickListener {
             val fileName: String = findViewById<TextView>(R.id.fileName).text.toString()
+            val dirName: String = findViewById<TextView>(R.id.dirName).text.toString()
             val helper = FilesHelper()
-            val file = helper.preparePrivateFile(applicationContext, fileName)
+            val file = helper.preparePrivateFile(applicationContext, fileName, if (dirName != "Dir") dirName else null)
+            if(!file.exists())
+                file.createNewFile()
 
             val fileData = findViewById<TextView>(R.id.fileContent).text.toString()
             helper.writeToPrivateFile(file, fileData.toByteArray(Charset.forName("UTF-8")))
             builder.setMessage("Saved")
 
             builder.create().show()
-            listFiles()
+            listFiles(applicationContext.filesDir)
         }
     }
-    private fun listFiles() {
-        val dir = applicationContext.filesDir
+    private fun listFiles(dir: File): String {
         val files = dir.listFiles();
+
 
         var str = ""
         if(files.isNullOrEmpty()) {
             findViewById<TextView>(R.id.fileList).text = str
-            return
+            return str
         }
         for (file in files) {
             str += file.toURI()
             str += '\n'
+            if (file.isDirectory()) {
+                val helper = FilesHelper()
+                val pfile = helper.preparePrivateFile(applicationContext, "", file.name)
+                str += listFiles(pfile)
+            }
         }
+
         findViewById<TextView>(R.id.fileList).text = str
+        return str
     }
 }
